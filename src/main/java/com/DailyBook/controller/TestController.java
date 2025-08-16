580e92fce65053e5e9ec97f85cbc225a76c32dc6
lostannu <icyanupam@gmail.com>
2025-08-16 18:31:39 +0530

    finally, now the login functionality... and the jwt toke authentication is working properly .Took hours to debug...

--- src/main/java/com/DailyBook/controller/TestController.java
+++ src/main/java/com/DailyBook/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.DailyBook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/test")
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
 
     @GetMapping("/public")
