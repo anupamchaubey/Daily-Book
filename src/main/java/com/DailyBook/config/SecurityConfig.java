8cb0a68ca5f3ddd81091e87e077659b0cf977f80
lostannu <icyanupam@gmail.com>
2025-08-16 18:19:04 +0530

    small fix in login functionality...

--- src/main/java/com/DailyBook/config/SecurityConfig.java
+++ src/main/java/com/DailyBook/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/api/test/public","/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
