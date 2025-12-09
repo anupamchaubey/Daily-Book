c0a0dc9f16f311d2b8932702de44d17469a8b2b1
anupamchaubey <icyanupam@gmail.com>
2025-12-09 18:27:39 +0530

    fixing errors

--- src/main/java/com/DailyBook/config/SecurityConfig.java
+++ src/main/java/com/DailyBook/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
-                "http://localhost:8080/",
+                "http://localhost:8080",
                 "http://192.168.1.80:80",
                 "https://daily-book.netlify.app",
                 "https://dailybook-x50p.onrender.com"
